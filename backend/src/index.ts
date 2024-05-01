0ffa6fa713a8c78a6e8f488e2a033bf7af4458e3
Amelie <amelie.barre.co@gmail.com>
2024-05-01 17:49:36 +0200

    setup auth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route"
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.API_PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/", (req, res) => {
+  res.send("Server is ready !");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
